293509b0d22cf38e4c8fa1c9fcffa5f6a4c9d083
Addisu <72408025+addispupi@users.noreply.github.com>
2021-03-18 22:20:24 +0300

    Add files via upload

---
+++ HomeWork3.1/main.cpp
@@ -0,0 +1,39 @@
+#include<graphics.h>
+#include<math.h>
+#include<iostream>
+
+using namespace std;
+main()
+{
+	int h,w,d, x0=150, y0=200;
+	cout<<"Enter the height: ";
+	cin>>h;
+	cout<<"Enter the width: ";
+	cin>>w;
+	cout<<"Enter the depth: ";
+	cin>>d;
+	
+	initwindow(400,600);
+	setcolor(YELLOW);
+	
+	// verticals
+    line(x0,y0,x0,y0+h);
+    line(x0+w,y0,x0+w,y0+h);
+    
+	// horizontals
+	line(x0,y0,x0+w,y0);
+	line(x0,y0+h,x0+w,y0+h);
+	
+	// depth vertical
+    line(x0-d,y0-(h/2),x0-d,y0+(h/2));
+    
+    // depth horizontal
+    line(x0-d,y0-(h/2),(x0-d)+w,y0-(h/2));
+    
+    // depth diagonals
+    line(x0-d,y0-(h/2),x0,y0);
+    line(x0-d,y0+(h/2),x0,y0+h);
+    line((x0-d)+w,y0-(h/2),x0+w,y0);
+	
+	getch();
+}
